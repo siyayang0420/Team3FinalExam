fa50a89806637d74e44ed630918e1bae1b77cf73
siyayang0420 <siyayang0420@live.com>
2019-12-10 01:21:56 -0800

    added initial file

---
+++ styles/styles.js
@@ -0,0 +1,22 @@
+import {StyleSheet} from 'react-native';
+var styles = StyleSheet.create({
+    //Deriq
+
+
+
+    //Nicole
+
+
+
+    //Anshylnn
+
+
+
+    //Hideki
+
+
+
+    //Siya
+})
+
+export default styles;
