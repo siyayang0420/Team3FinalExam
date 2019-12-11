fb6cc2e0ee22c6850532a34ba5499588d44d1f02
ashlynnchen <ashkiwi0607@gmail.com>
2019-12-11 11:03:27 -0800

    need help!

--- comps/stories.js
+++ comps/stories.js
@@ -2,3 +2,14 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
+import DefaultImage from './DefaultImage';
+
+
+storiesOf("default image", module)
+    .add("DefaultImage", ()=>{
+        return(
+            <DefaultImage 
+            title="Whooop"
+            />
+        )
+    });
