9b0395be90e1f2b649a12537784bf79f1e06e360
Nicole <nicole.ll.930529@gmail.com>
2019-12-11 12:29:34 -0800

    profile done

--- comps/Profile.js
+++ comps/Profile.js
@@ -7,19 +7,24 @@ import DefaultImage from './DefaultImage';
 
 function Profile(props){
     const [title, setTitle] = useState('');
+    const [placeholderTxt, SetPlaceholderTxt] = useState('');
     return(
         <View style={{marginTop:100}}>
              <DefaultImage style={{marginLeft: 50}}/>
 
             <View style={{alignItems:"center", justifyContent:"center", marginTop:60}}>
                 <Text>Username</Text>
-                <Default_TextInput />
+                <Default_TextInput 
+                    placeholderTxt={"ttt"}
+                />
                 <Text>Image URL</Text>
                 <Default_TextInput 
+                    placeholderTxt={"ddd"}
                 />
                 
                 <ButtonAnimation 
                 title={"save"}
+
 
                 />
             </View>
