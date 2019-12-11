a6e4f685698af051831a1232e77f37e9ed6a8418
HidekiMurata <ilovrbed0324@gmail.com>
2019-12-11 10:42:16 -0800

    the first pull request for default-message

---
+++ comps/Default_Message.js
@@ -0,0 +1,14 @@
+import React {useState, useEffect} from 'react';
+import {Text, View, Animated} from 'react-native';
+import { useState } from 'react';
+
+function Message(){
+    return(
+        <View animated>
+
+        </View>
+    )
+}
+
+
+export default DefaultMessage;
