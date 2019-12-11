1c69c116897f7f28432d2a939944b3f01bb7c679
HidekiMurata <ilovrbed0324@gmail.com>
2019-12-11 12:05:58 -0800

    check

--- comps/stories.js
+++ comps/stories.js
@@ -3,8 +3,19 @@ import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
 import Default_TextInput from './Default_TextInput';
+import Default_Message from './Default_Message';
 
 storiesOf('final_comps', module)
   .add('Default TextInput', ()=>{
     return<Default_TextInput/>
   })
+
+  
+storiesOf('final_comps', module)
+  .add('Default Message', ()=>{
+    return<Default_Message/>
+})
+
+
+
+
