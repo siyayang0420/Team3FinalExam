63d74724a2671d85e104005246223dd218deefe5
siyayang0420 <siyayang0420@live.com>
2019-12-11 11:17:27 -0800

    added comps

--- comps/stories.js
+++ comps/stories.js
@@ -3,19 +3,25 @@ import { Text } from 'react-native';
 import { storiesOf } from '@storybook/react-native';
 import DefaultImage from './DefaultImage';
 import ButtonAnimation from './ButtonAnimation';
-storiesOf('final', module)
-  .add('button', () => {
-   return <ButtonAnimation />
-  });
-  storiesOf('final_comps', module)
-  .add('Default TextInput', ()=>{
-    return<Default_TextInput/>
-  });
-    .add("DefaultImage", ()=>{
-        return(
-            <DefaultImage 
-            title="Whooop"
-            />
-        )
-    });
+import Default_TextInput from './Default_TextInput';
 
+    
+
+    storiesOf('final_comps', module)
+    .add('Default TextInput', ()=>{
+      return<Default_TextInput/>
+    })
+    .add('Defaut Message Board', ()=>{
+      return<Defaut_Message_Board/>
+    })
+    .add('button', () => {
+      return <ButtonAnimation />
+     })
+
+     .add("DefaultImage", ()=>{
+      return
+          <DefaultImage 
+          title="Whooop"
+          />
+      
+  });
