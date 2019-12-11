b0f7d2a9f71c645a6264ae9452db2e7f14da7618
siyayang0420 <siyayang0420@live.com>
2019-12-11 10:13:43 -0800

    finished textinput

--- comps/Default_TextInput.js
+++ comps/Default_TextInput.js
@@ -2,32 +2,37 @@ import React, { useEffect, useState } from "react";
 import { View, Text, TextInput, Animated } from "react-native";
 
 function Default_TextInput() {
-    const [widScale] = useState(new Animated.Value("0%"));
+    const [widScale] = useState(new Animated.Value(0));
     useEffect(()=>{
         Animated.timing(
             widScale,
             {
-                toValue:"100%",
+                toValue:350,
                 duration:1000
             }
         ). start();
     }, []);
   return (
-    <View >
-      <Animated.View
-      style={{
+    <View style={{
         justifyContent:'center',
         alignItems:'center',
+    }}>
+      <Animated.View
+      style={{
         marginTop:300,
-        width:"100%"
+        width:widScale
     }}
       >
         <TextInput
           style={{
             width: 350,
             height: 50,
-            backgroundColor: "#d3d3d3",
+            backgroundColor: "#ffffff",
             borderRadius:10,
+            shadowColor:'#000000',
+            shadowOffset:{x:3,y:3},
+            shadowRadius:10,
+            shadowOpacity:0.1
           }}
           placeholder={"Default placeholder"}
         />
--- comps/stories.js
+++ comps/stories.js
@@ -2,3 +2,9 @@ import React from 'react';
 import { Text } from 'react-native';
 
 import { storiesOf } from '@storybook/react-native';
+import Default_TextInput from './Default_TextInput';
+
+storiesOf('final_comps', module)
+  .add('Default TextInput', ()=>{
+    return<Default_TextInput/>
+  })
